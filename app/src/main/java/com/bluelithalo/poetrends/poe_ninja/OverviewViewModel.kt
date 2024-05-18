d5a8049083b6afd0abb1b1bf261c8cf264772104
Alexei Korol <korolalexei@gmail.com>
2024-05-18 16:05:51 +0400

    Changed league to Necropolis

--- app/src/main/java/com/bluelithalo/poetrends/poe_ninja/OverviewViewModel.kt
+++ app/src/main/java/com/bluelithalo/poetrends/poe_ninja/OverviewViewModel.kt
@@ -15,7 +15,7 @@ import java.util.*
 
 class OverviewViewModel : ViewModel()
 {
-    private var leagueId: String = "Affliction"
+    private var leagueId: String = "Necropolis"
     private var overviewType: Overview.Type = Overview.Type.CURRENCY
     private var searchQuery: String = ""
 
